33bc0e81cd7ca4ea285123207435b5da02984be4
LinTx <ztj@jjcbw.com>
2026-06-03 19:23:09 +0800

    二维码生成库内置进exe

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,8 +18,42 @@ namespace endfield_player_position_display
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveEmbeddedAssembly;
             TryUseUtf8ConsoleEncoding();
             base.OnStartup(e);
+        }
+
+        private static Assembly ResolveEmbeddedAssembly(object sender, ResolveEventArgs args)
+        {
+            string assemblyName = new AssemblyName(args.Name).Name;
+            if (!string.Equals(assemblyName, "QRCoder", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+
+            Assembly currentAssembly = typeof(App).Assembly;
+            using (Stream stream = currentAssembly.GetManifestResourceStream("EmbeddedAssemblies.QRCoder.dll"))
+            {
+                if (stream == null)
+                {
+                    return null;
+                }
+
+                byte[] assemblyBytes = new byte[stream.Length];
+                int offset = 0;
+                while (offset < assemblyBytes.Length)
+                {
+                    int read = stream.Read(assemblyBytes, offset, assemblyBytes.Length - offset);
+                    if (read == 0)
+                    {
+                        break;
+                    }
+
+                    offset += read;
+                }
+
+                return Assembly.Load(assemblyBytes);
+            }
         }
 
         private static void TryUseUtf8ConsoleEncoding()
